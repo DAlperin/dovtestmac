1c69916ba77606f1f9d178710d341069db299a52
Dov <dzalperin@gmail.com>
2016-11-03 08:35:09 -0400

    Initial Commit

---
+++ New test/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  New test
+//
+//  Created by Dov Alperin on 11/3/16.
+//  Copyright © 2016 Dov Alperin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ New test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  New test
+//
+//  Created by Dov Alperin on 11/3/16.
+//  Copyright © 2016 Dov Alperin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
